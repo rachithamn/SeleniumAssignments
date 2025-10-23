fec5cd67eaae63dfcb4c247eac9444e64b7167e5
Rachitha M.N <94373147+rachithamn@users.noreply.github.com>
2025-10-23 19:03:47 +0530

    Update BrowserLaunch.java

--- week1/day1/BrowserLaunch.java
+++ week1/day1/BrowserLaunch.java
@@ -1,17 +1,1 @@
-package week2.day1;
 
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class BrowserLaunch {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ChromeDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https:www.facebook.com/");
-		
-		
-
-	}
-
-}

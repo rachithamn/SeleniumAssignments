8961d395d2093ee1d732c3448d61ecd149cdcde7
Rachitha M.N <94373147+rachithamn@users.noreply.github.com>
2025-10-30 16:33:17 +0530

    Update Button.java

--- week3/day2/Button.java
+++ week3/day2/Button.java
@@ -1,8 +1,9 @@
 package week3.day2;
 
-public class Button {
+public class Button extends WebElements {
 	// submit() method
 	void submit() {
+		System.out.println("Button submitted!");
 		
 	}
 	public static void main(String[] args) {

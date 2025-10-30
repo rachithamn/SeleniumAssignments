18c3d76fd4cf5c3d7a0fc9185fd25817ddffd0d0
Rachitha M.N <94373147+rachithamn@users.noreply.github.com>
2025-10-30 16:32:51 +0530

    Update CheckBoxButton.java

--- week3/day2/CheckBoxButton.java
+++ week3/day2/CheckBoxButton.java
@@ -1,14 +1,14 @@
 package week3.day2;
 
-public class CheckBoxButton {
+public class CheckBoxButton extends Button {
 	/**
 	 * - Develop a new class named CheckBoxButton that inherits from the Button subclass.
 - In the CheckBoxButton class, introduce a method named clickCheckButton().
 	 * @param args
 	 */
-	void clickCheckButton() {
-		
-	}
+	 public void clickCheckButton() {
+	        System.out.println("Checkbox button clicked!");
+	    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

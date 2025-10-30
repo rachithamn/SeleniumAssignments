859025444675c255afe96ede81d8d7cae7f0fa78
Rachitha M.N <94373147+rachithamn@users.noreply.github.com>
2025-10-30 16:29:41 +0530

    Update Elements.java

--- week3/day2/Elements.java
+++ week3/day2/Elements.java
@@ -6,6 +6,35 @@ public class Elements extends Button {
 		 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("---- WebElement Example ----");
+        WebElements element = new WebElements();
+        element.click();
+        element.setText("Generic Element");
+
+        System.out.println("\n---- Button Example ----");
+        Button button = new Button();
+        button.click();
+        button.submit();
+
+        System.out.println("\n---- TextField Example ----");
+        TextField field = new TextField();
+        field.getText();
+
+        System.out.println("\n---- CheckBoxButton Example ----");
+        CheckBoxButton checkBox = new CheckBoxButton();
+        checkBox.click();
+        checkBox.clickCheckButton();
+        checkBox.submit();
+
+        System.out.println("\n---- RadioButton Example ----");
+        RadioButton radio = new RadioButton();
+        radio.click();
+        radio.selectRadioButton();
+
+        System.out.println("\n---- Elements Class Example ----");
+        Elements elementObj = new Elements();
+        elementObj.click();
+        elementObj.submit();
 
 	}
 
